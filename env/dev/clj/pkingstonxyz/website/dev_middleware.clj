cbae473d85a925aa839a8605fe2f7f15b2181b4f
Patrick Kingston <patrick@pkingston.xyz>
2022-11-12 00:58:31 -0500

    First commit

---
+++ env/dev/clj/pkingstonxyz/website/dev_middleware.clj
@@ -0,0 +1,5 @@
+(ns pkingstonxyz.website.dev-middleware)
+
+(defn wrap-dev [handler _opts]
+  (-> handler
+      ))
---
+++ test/clj/pkingstonxyz/website/core_test.clj
@@ -0,0 +1,8 @@
+(ns pkingstonxyz.website.core-test
+  (:require
+    [pkingstonxyz.website.test-utils :as utils]
+    [clojure.test :refer :all]))
+
+(deftest example-test
+  (is (= 1 2)))
+
